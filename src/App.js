e4a8c3667c8857a1000323ccf9db102a4e7c71d7
Trevor Seibel <trevor.seibel@gmail.com>
2018-03-21 18:32:18 -0400

    Initial layout of navbar and movie bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    //AJAX request to go here
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="header">
+          <div className="navbar">
+            <div className="title">
+              <h1>Notflix</h1>
+            </div>
+            <div className="navbarButtons" />
+            <div className="searchBar">
+              <form>
+                <input type="text" placeholder="Enter a search term"/>
+              </form>
+            </div>
+            <div className="navbarLinks">
+              <a>New Releases</a>
+              <a>Top Rated</a>
+              <a>Coming Soon</a>
+              <a>Hire Me!</a>
+            </div>
+            
+            
+            </div>
+
+          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="navbarSpacing" />
+        <h2 className="App-intro">
+          <div className="category">Current New Releases</div>
+          <div className="movieCarousel">movies movies movies</div>
+        </h2>
       </div>
     );
   }
